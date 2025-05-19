f96e7ac3128ef4d4dac07fd8d033f954a7584f11
dlalloyd <dylanlloydparker92@outlook.com>
2025-05-19 10:43:53 +0100

    fixed interface

---
+++ MuseumTourSystem.BusinessLogic/Interfaces/IMuseumTourRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuseumTourSystem.Models.Models;
+
+namespace MuseumTourSystem.BusinessLogic.Interfaces
+{
+    public interface IMuseumTourRepository
+    {
+
+            #region Tour Operations
+
+            void AddTour(Tours tour);
+
+            void RemoveTour(Tours tour);
+
+            Tours GetTourById(string id);
+
+            IEnumerable<Tours> GetAllTours();
+
+            #endregion
+
+            #region City Operations
+
+            void AddCity(City city);
+
+            void RemoveCity(City city);
+
+            City GetCityById(string id);
+
+            IEnumerable<City> GetAllCities();
+
+            #endregion
+
+            #region Museum Visit Operations
+
+            void AddMuseumVisit(MuseumVisit museumVisit);
+
+            void RemoveMuseumVisit(MuseumVisit museumVisit);
+
+            MuseumVisit GetMuseumVisitById(string id);
+
+            IEnumerable<MuseumVisit> GetAllMuseumVisits();
+
+            #endregion
+
+            #region Member Operations
+
+            void AddMember(Member member);
+
+            void RemoveMember(Member member);
+
+            Member GetMemberById(string id);
+
+            Member GetMemberByBookingNumber(string bookingNumber);
+
+            IEnumerable<Member> GetAllMembers();
+
+            #endregion
+
+            void SaveChanges();
+            void LoadData();
+        }
+
+    }
+
