853a44179f13d7823de6131417a18a8e5639aed2
dlalloyd <dylanlloydparker92@outlook.com>
2025-05-19 10:47:02 +0100

    added service class to manage tours

---
+++ MuseumTourSystem.BusinessLogic/Services/MuseumTourService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuseumTourSystem.Models.Models;
+using MuseumTourSystem.BusinessLogic.Interfaces;
+
+namespace MuseumTourSystem.BusinessLogic.Services
+{
+    public class MuseumTourService
+    {
+    }
+}
